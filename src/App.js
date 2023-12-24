5abf4e984c18a4738cf2f2c15ad4644b9672920d
mahdee10 <diranimahdi2002@gmail.com>
2023-12-24 16:41:44 +0100

    movies

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+      <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@700&family=Jomhuria&family=Open+Sans:wght@300&display=swap" rel="stylesheet"></link>
       <Navbar></Navbar>
       <Home></Home>
     </div>
