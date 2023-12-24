a4a48ba9b752819fab462dcab638302f68ccba25
mahdee10 <diranimahdi2002@gmail.com>
2023-12-24 15:55:51 +0100

    movies

--- src/components/homeSwiper.js
+++ src/components/homeSwiper.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from 'react';
 const HomeSwiper = () => {
     const [backgroundIndex, setBackgroundIndex] = useState(0);
     const [movieData, setMovieData] = useState(null);
+    const [preloadedImages, setPreloadedImages] = useState([]);
     const [size, setSize] = useState(null);
     const apiKey = '75a499747ddeaacd5a5ca88536c09337';
 
@@ -25,6 +26,13 @@ const HomeSwiper = () => {
 
         fetchData();
     }, []);
+    
+
+    useEffect(()=>{
+        if (movieData && movieData.length > 0) {
+        preloadImages();
+        }
+    },[movieData])
 
     useEffect(() => {
         if (movieData && movieData.length > 0) {
@@ -34,7 +42,7 @@ const HomeSwiper = () => {
                 setBackgroundIndex((prevIndex) =>
                     prevIndex === movieData.length - 1 ? 0 : prevIndex + 1
                 );
-            }, 5000);
+            }, 4000);
             return () => clearInterval(interval);
         }
     }, [backgroundIndex, movieData]);
@@ -53,22 +61,40 @@ const HomeSwiper = () => {
         };
     }, []);
 
+    function preloadImages() {
+        const imgs = movieData.map((movie) => {
+            const imgP = new Image();
+            imgP.src = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
+            imgP.onload = () => console.log(`Poster image preloaded: ${imgP.src}`);
+    
+            const imgB = new Image();
+            imgB.src = `https://image.tmdb.org/t/p/original/${movie.backdrop_path}`;
+            imgB.onload = () => console.log(`Backdrop image preloaded: ${imgB.src}`);
+    
+            return { imgP, imgB };
+        });
+    
+        setPreloadedImages(imgs);
+    }
+    
+
     return (
         <div
-            className={`w-[${size}px] flex justify-end sm:items-end z-0 mb-10 transition-animation`}
-            style={{
-                height: "87vh",
-                backgroundSize: size <= 768 ? "100% 100%" : "cover",
-                backgroundRepeat:"no-repeat",
-                backgroundImage: size <= 768
-                    ? movieData && movieData.length > 0
-                        ? `url(https://image.tmdb.org/t/p/original/${movieData[backgroundIndex].poster_path})`
+        className={`w-[${size}px] flex justify-end sm:items-end z-0 mb-10 transition-animation`}
+        style={{
+            height: '87vh',
+            backgroundSize: size <= 768 ? '100% 100%' : 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundImage:
+                size <= 768
+                    ? movieData && movieData.length > 0 && preloadedImages.length > 0
+                        ? `url(${preloadedImages[backgroundIndex].imgP.src})`
                         : 'none'
-                    : movieData && movieData.length > 0
-                        ? `url(https://image.tmdb.org/t/p/original/${movieData[backgroundIndex].backdrop_path})`
-                        : 'none'
-            }}
-        />
+                    : movieData && movieData.length > 0 && preloadedImages.length > 0
+                    ? `url(${preloadedImages[backgroundIndex].imgB.src})`
+                    : 'none',
+        }}
+    />
     );
 };
 
