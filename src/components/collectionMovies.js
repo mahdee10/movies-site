d0eeb02826b227b375928c36f9278143f3796b75
mahdee10 <diranimahdi2002@gmail.com>
2023-12-31 00:15:04 +0100

    movies

---
+++ src/components/collectionMovies.js
@@ -0,0 +1,19 @@
+import Poster from "./moviePoster";
+
+export default function MoviesCollection({movies,title}){
+    return(
+        movies && movies.length>0? (
+            <div className="flex sm:justify-center sm:w-3/4 sm:mt-32 mt-20 w-full sm:p-0 p-3">
+                <div className="flex flex-wrap sm:justify-center justify-between w-full">
+                    {
+                        movies.map((movie,index)=>(
+                            <Poster movie={movie} key={index} classContent={"sm:w-40 mt-2 sm:h-60 h-42 w-[48%] shrink-0 sm:mr-3"} source={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}></Poster>
+                        ))
+                    }
+                </div>
+            </div>
+        ):(
+            <div className="text-white sm:text-4xl text-2xl mt-32 text-center">No Movies in the {title}</div>
+        )
+    )
+}
