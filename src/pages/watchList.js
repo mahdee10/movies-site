c4386c9322b9ddc2d834c48648532daacc060b77
mahdee10 <diranimahdi2002@gmail.com>
2023-12-31 00:06:14 +0100

    movies

--- src/pages/watchList.js
+++ src/pages/watchList.js
@@ -6,11 +6,11 @@ export default function WatchList(){
     return(
         
             watchLater && watchLater.length>0? (
-                <div className="flex sm:justify-center sm:w-3/4 sm:mt-32 mt-20 w-full sm:p-0 p-2">
+                <div className="flex sm:justify-center sm:w-3/4 sm:mt-32 mt-20 w-full sm:p-0 p-3">
                     <div className="flex flex-wrap sm:justify-center justify-between w-full">
                         {
                             watchLater.map((movie,index)=>(
-                                <Poster movie={movie} key={index} classContent={"sm:w-40 sm:h-60 h-42 w-1/2 shrink-0 sm:mr-3"} source={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}></Poster>
+                                <Poster movie={movie} key={index} classContent={"sm:w-40 mt-2 sm:h-60 h-42 w-1/2 shrink-0 sm:mr-3"} source={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}></Poster>
                             ))
                         }
                     </div>
