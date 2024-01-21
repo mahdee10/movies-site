00113283a290b45468ae6f5fecafa4a58a5ca878
mahdee10 <diranimahdi2002@gmail.com>
2024-01-21 18:18:16 +0100

    k

--- src/pages/movie/movie.js
+++ src/pages/movie/movie.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import MovieBP from "../../components/movie/movieBP";
 import MovieDetails from "../../components/movie/movieDetails";
 import Trailer from "../../components/movie/movieTrailer";
@@ -7,16 +7,15 @@ import Cast from "../../components/movie/cast";
 import SimilarMovies from "../../components/movie/similarMovies";
 
 export default function Movie() {
-    const [movie, setMovie] = useState()
-    const { id } = useParams()
+    const [movie, setMovie] = useState();
+    const { id } = useParams();
     const apiKey = '75a499747ddeaacd5a5ca88536c09337';
-   
 
     useEffect(() => {
-        setMovie(null)
+        setMovie(null);
         const fetchData = async () => {
             try {
-                const detailsUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=75a499747ddeaacd5a5ca88536c09337&append_to_response=credits`;
+                const detailsUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&append_to_response=credits`;
                 const videosUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}`;
 
                 const [detailsResponse, videosResponse] = await Promise.all([
@@ -29,8 +28,6 @@ export default function Movie() {
                     videosResponse.json(),
                 ]);
 
-        
-
                 setMovie({ ...detailsData, videos: videosData.results });
             } catch (error) {
                 console.error('Error:', error);
@@ -40,21 +37,50 @@ export default function Movie() {
         fetchData();
     }, [id, apiKey]);
 
+    useEffect(() => {
+        // Update meta tags for sharing when movie data changes
+        if (movie) {
+            updateMetaTags(movie);
+        }
+    }, [movie]);
+
+    const updateMetaTags = (movie) => {
+        const ogImageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+        const ogTitle = movie.title;
+        const ogDescription = movie.overview;
+        const ogUrl = window.location.href;
+
+        document.title = ogTitle;
+
+        const metaTags = document.head.querySelectorAll('meta[property^="og:"]');
+        metaTags.forEach((tag) => tag.remove());
+
+        const metaTag = (property, content) => {
+            const meta = document.createElement('meta');
+            meta.setAttribute('property', property);
+            meta.content = content;
+            document.head.appendChild(meta);
+        };
+
+        metaTag('og:title', ogTitle);
+        metaTag('og:description', ogDescription);
+        metaTag('og:image', ogImageUrl);
+        metaTag('og:url', ogUrl);
+    };
+
     return (
         <div className="pb-10">
-            {
-                movie ? (
-                    <>
-                        <MovieBP poster={movie.poster_path} background={movie.backdrop_path}></MovieBP>
-                        <MovieDetails movie={movie}></MovieDetails>
-                        <Trailer movie={movie}></Trailer>
-                        <Cast movie={movie}></Cast>
-                        <SimilarMovies id={movie.id}></SimilarMovies>
-                    </>
-                ) : (
-                    null
-                )
-            }
+            {movie ? (
+                <>
+                    <MovieBP poster={movie.poster_path} background={movie.backdrop_path}></MovieBP>
+                    <MovieDetails movie={movie}></MovieDetails>
+                    <Trailer movie={movie}></Trailer>
+                    <Cast movie={movie}></Cast>
+                    <SimilarMovies id={movie.id}></SimilarMovies>
+                </>
+            ) : (
+                null
+            )}
         </div>
-    )
+    );
 }
