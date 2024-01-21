408ff84bce190574e7d052e8100ef87d4fdc55ca
mahdee10 <diranimahdi2002@gmail.com>
2024-01-21 15:58:25 +0100

    s

--- src/components/actor/actorDescription.js
+++ src/components/actor/actorDescription.js
@@ -2,7 +2,7 @@ export default function ActorDescription({ actor }) {
     return (
         <div className="flex sm:flex-row flex-col  sm:pt-10 ">
             <div className=" sm:w-[48%] sm:pt-0 justify-center">
-                <img className="sm:rounded-xl sm:w-[60%] h-[400px] sm:mt-0 mt-3" src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt=""></img>
+                <img className="sm:rounded-xl sm:w-[60%] sm:h-[400px] h-[500px] sm:mt-0 mt-3" src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt=""></img>
             </div>
             <div className="flex flex-col sm:items-start items-center sm:w-[48%]   sm:pt-0 pt-10 px-2 ">
                 {/* <p className="text-white  sm:w-[50%] sm:mt-0 mt-1"><span className="text-voilet">Release Year:</span>{actor}</p> */}
