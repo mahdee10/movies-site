db382cbb8ec075c75fae15a3353e81f1d77e16ab
Mahdi El Dirani <diranimahdi2002@gmail.com>
2024-02-15 20:31:47 +0100

    s

--- src/pages/watchList.js
+++ src/pages/watchList.js
@@ -4,7 +4,7 @@ import { useWatchlaterContext } from "../context/watchLaterContext"
 export default function WatchList() {
     const { watchLater } = useWatchlaterContext()
     return (
-        <div className="sm:pt-10">
+        <div className="sm:pt-[100px]">
             <MoviesCollection title={"Watch List"} movies={watchLater}></MoviesCollection>
         </div>
 
