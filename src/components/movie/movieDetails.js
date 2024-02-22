e207ce3aaf0153bafd54c70798580f2d4eed9c36
Mahdi El Dirani <diranimahdi2002@gmail.com>
2024-02-22 16:29:12 +0100

    share

--- src/components/movie/movieDetails.js
+++ src/components/movie/movieDetails.js
@@ -1,3 +1,4 @@
+import { WhatsappShareButton } from "react-share";
 import { useWatchlaterContext } from "../../context/watchLaterContext"
 import watchIcon from "../../imgs/fav.png"
 export default function MovieDetails({ movie }) {
@@ -7,11 +8,18 @@ export default function MovieDetails({ movie }) {
     const hours = Math.floor(runtimeInMinutes / 60);
     const minutes = runtimeInMinutes % 60;
 
+    const movieUrl = `https://cinemaxxxx.netlify.app/movie/${movie.id}`;
+    const message = `Check out this movie: ${movie.title}`;
+    const imageUrl = movie.previewPhotoUrl;
+  
     return (
         <div className="sm:pt-32 pt-10 sm:px-20 px-2">
             <div className="flex sm:flex-row flex-col  justify-between ">
                 <h1 className="sm:text-5xl text-4xl text-white font-black sm:text-start sm:order-1 order-2 sm:pt-0 pt-10">{movie.original_title}</h1>
-                <a className="text-white" href="whatsapp://send?text=">Start a WhatsApp Chat</a> 
+                {/* <a className="text-white" href="whatsapp://send?text=">Start a WhatsApp Chat</a>  */}
+                <WhatsappShareButton url={movieUrl} title={message} separator=" - " className="share-button text-white">
+                <img src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`} alt="WhatsApp" />
+                </WhatsappShareButton>
                 <div onClick={() => { isInWatchList(movie) ? removeItemWatchLater(movie) : addItemToWatchLater(movie) }} className={`${isInWatchList(movie) ? "bg-[red]" : "bg-voilet"} cursor-pointer text-white rounded-xl p-2  w-fit watchList flex justify-between items-center sm:order-2 order-1 sm:self-auto self-end `}>
                     <img src={watchIcon} className="w-5 h-5" alt="watch"></img>
                     <p className="sm:text-base text-xs">{isInWatchList(movie) ? "Remove from WatchLater" : "Add to WatchLater"}</p>
