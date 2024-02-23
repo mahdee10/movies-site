84c60f5bb94f0dbcf82c92a1e7d72a4cfb9fdbc3
Mahdi El Dirani <diranimahdi2002@gmail.com>
2024-02-23 15:31:42 +0100

    h

--- src/components/movie/movieTrailer.js
+++ src/components/movie/movieTrailer.js
@@ -1,30 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+
 
 export default function Trailer({ movie }) {
-    const [inViewport, setInViewport] = useState(false);
-    const videoRef = useRef(null);
-
-    useEffect(() => {
-        const onScroll = () => {
-            const rect = videoRef.current.getBoundingClientRect();
-            const isInViewport = (
-                rect.top >= 0 &&
-                rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-            );
-            setInViewport(isInViewport);
-        };
-
-        window.addEventListener('scroll', onScroll);
-        onScroll(); // Call to check immediately on mount
-
-        return () => {
-            window.removeEventListener('scroll', onScroll);
-        };
-    }, []);
-
-
 
     const getTrailerKey = () => {
         const trailer = movie.videos.find(video => video.type === 'Trailer');
@@ -35,11 +11,10 @@ export default function Trailer({ movie }) {
         <div className="flex justify-center sm:pt-32 pt-10 sm:px-20 px-2">
             {getTrailerKey() ? (
                 <iframe
-                ref={videoRef}
                     className="sm:w-3/4 w-full sm:h-[500px] h-[200px]"
-                    src={`https://www.youtube.com/embed/${getTrailerKey()}${inViewport ? '?autoplay=1' : ''}`}
+                    src={`https://www.youtube.com/embed/${getTrailerKey()}`}
                     title="YouTube Video"
-                    frameBorder="0"
+
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
                 ></iframe>
